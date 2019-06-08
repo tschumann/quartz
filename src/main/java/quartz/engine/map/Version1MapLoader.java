db2c2bb6ec83a340a1e96f75018aed5d34e619d8
tschumann <schumann.tom@gmail.com>
2019-06-08 21:39:08 +1000

    Added Version1MapLoader tests.

--- src/main/java/quartz/engine/map/Version1MapLoader.java
+++ src/main/java/quartz/engine/map/Version1MapLoader.java
@@ -11,13 +11,15 @@ public class Version1MapLoader extends BaseMapLoader {
 
     public void loadMap(List<String> lines) {
         // start from line 1 because line 0 is the version information
+        // TODO: should probably fix this to not rely on the version string being element 0
         for (int i = 1; i < lines.size(); i++) {
+            int lineNumber = i + 1;
             String line = lines.get(i);
             String parts[] = line.split(",");
 
             if (parts.length != ENTITY_PARTS_COUNT) {
                 // TODO: ideally put line number in here
-                this.logger.Error("Entity declaration was " + parts.length + " items: expected " + ENTITY_PARTS_COUNT);
+                this.logger.Error("Entity declaration on line " + lineNumber + " was " + parts.length + " items: expected " + ENTITY_PARTS_COUNT);
                 continue;
             }
 
@@ -29,6 +31,8 @@ public class Version1MapLoader extends BaseMapLoader {
                 entity.setAngles(new Vector(new Float(parts[4]), new Float(parts[5]), new Float(parts[6])));
                 this.logger.Debug("Adding " + entity);
                 this.entities.add(entity);
+            } else {
+                this.logger.Error("Unable to instantiate " + parts[0] + " on line " + lineNumber);
             }
         }
     }
